4bcb02897811254343faa8e6d5ade950ac18f969
adrianparedes477 <100713764+adrianparedes477@users.noreply.github.com>
2022-08-09 22:44:58 -0300

    Add files via upload

---
+++ Pre-aceleracion-CSharp/ViewModels/DetailMovieViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Disney.ViewModels
+{
+    public class DetailMovieViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public DateTime CreationDate { get; set; }
+        public float Score { get; set; }
+        public List<CharactersViewModel> Characters { get; set; }
+    }
+}
