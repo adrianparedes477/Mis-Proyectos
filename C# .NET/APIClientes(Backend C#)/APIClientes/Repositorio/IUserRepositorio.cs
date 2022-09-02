897d62bad8b56b2cabe95f83b433e4bf1d697eea
adrianparedes477 <100713764+adrianparedes477@users.noreply.github.com>
2022-09-02 16:38:02 -0300

    Add files via upload

---
+++ C# .NET/APIClientes(Backend C#)/APIClientes/Repositorio/IUserRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using APIClientes.Modelos;
+using System.Threading.Tasks;
+
+namespace APIClientes.Repositorio
+{
+    public interface IUserRepositorio
+    {
+        Task<string> Register(User user, string password);
+        Task<string> Login(string userName, string password);
+        Task<bool> UserExiste(string username);
+    }
+}
