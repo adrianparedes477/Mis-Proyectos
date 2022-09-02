45638e6e4822bfb86f5b03ab1e8db99aee37cc70
adrianparedes477 <100713764+adrianparedes477@users.noreply.github.com>
2022-09-02 16:34:06 -0300

    Delete userspagination directory

--- userspagination/src/app/usuarios/MatPaginatorIntlEs.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-import { MatPaginatorIntl } from '@angular/material/paginator';
-
-
-@Injectable()
-export class MatPaginatorIntlEs extends MatPaginatorIntl {
-    override itemsPerPageLabel = 'Registros por Pagina';
-}
